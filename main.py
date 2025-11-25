d4177378a695f585de5da984fe9791a46dbdc5c5
Elias Evjen Hartmark <elias.hartmark@gmail.com>
2025-11-25 21:05:53 +0100

    Add 100+ iteration test results and ny specification system for ABZ rover.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import nuXmvHandler
 
 MODEL = "gpt-5-chat-latest"  # You can also try: "gpt-5" "gpt-5-chat-latest" "gpt-4-turbo" "gpt-5-reasoning"
 NUM_ITERATIONS = 100 # Number of iterations for the entire batch process
-TEMPERATURE = 1  # Adjust temperature for variability in responses
+TEMPERATURE = 0  # Adjust temperature for variability in responses
 EQUIVALENCE_HANDLER = nuXmvHandler.check_equivalence_rover
 
 
